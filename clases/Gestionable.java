3a438e8a94fcb56559cbd8284f4c179839f7af52
PabloBernalVilana <pabevi@hotmail.com>
2021-07-19 17:15:50 +0200

    Rama desarrollo clase abstracta
    Rama desarrollo clase abstracta

--- clases/Gestionable.java
+++ clases/Gestionable.java
@@ -1,5 +1,19 @@
 package ejercicio4.clases;
+
 // Clase Abstracta Padre de Alumno y Profesor
 public abstract class Gestionable {
+	protected String Nombre;
+	protected int Edad;
+	protected char Sexo;
+
+	// Constructor con todos los Atributos
+	public Gestionable(String nombre, int edad, char sexo) {
+		Nombre = nombre;
+		Edad = edad;
+		Sexo = sexo;
+	}
+
+	// Metodo para controlar asistencia
+	public abstract void controlAsistencia();
 
 }
