7d7bc73903611b462097ad23ea5ce58271ab6e53
PabloBernalVilana <pabevi@hotmail.com>
2021-07-19 17:09:24 +0200

    Estructura de la aplicacion

---
+++ clases/Alumno.java
@@ -0,0 +1,5 @@
+package ejercicio4.clases;
+// Clase Alumno
+public class Alumno {
+
+}
---
+++ clases/Gestionable.java
@@ -0,0 +1,5 @@
+package ejercicio4.clases;
+// Clase Abstracta Padre de Alumno y Profesor
+public abstract class Gestionable {
+
+}
